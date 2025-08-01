2ea35382088d45dd547466522f4e73491826a735
phi <phi.friday@gmail.com>
2025-08-02 00:41:07 +0900

    fix: simple_client

--- src/socketio-stubs/simple_client.pyi
+++ src/socketio-stubs/simple_client.pyi
@@ -1,27 +1,52 @@
 import types
+from threading import Event
+from typing import Any, ClassVar, Literal
+
 from _typeshed import Incomplete
-from socketio import Client as Client
-from socketio.exceptions import DisconnectedError as DisconnectedError, SocketIOError as SocketIOError, TimeoutError as TimeoutError
+from socketio import Client
+from socketio._types import DataType
+from typing_extensions import Self
 
 class SimpleClient:
-    client_class = Client
-    client_args: Incomplete
-    client_kwargs: Incomplete
-    client: Incomplete
+    client_class: ClassVar[type[Client]]
+    client_args: tuple[Any, ...]
+    client_kwargs: dict[str, Any]
+    client: Client | None
     namespace: str
-    connected_event: Incomplete
+    connected_event: Event
     connected: bool
-    input_event: Incomplete
-    input_buffer: Incomplete
-    def __init__(self, *args, **kwargs) -> None: ...
-    def connect(self, url, headers={}, auth=None, transports=None, namespace: str = '/', socketio_path: str = 'socket.io', wait_timeout: int = 5) -> None: ...
+    input_event: Event
+    input_buffer: list[list[Any]]
+    def __init__(self, *args: Any, **kwargs: Any) -> None: ...
+    def connect(
+        self,
+        url: str,
+        headers: dict[Incomplete, Incomplete] = ...,
+        auth: Incomplete | None = ...,
+        transports: Literal["polling", "websocket"] | None = ...,
+        namespace: str = ...,
+        socketio_path: str = ...,
+        wait_timeout: int = ...,
+    ) -> None: ...
     @property
-    def sid(self): ...
+    def sid(self) -> str | None: ...
     @property
-    def transport(self): ...
-    def emit(self, event, data=None): ...
-    def call(self, event, data=None, timeout: int = 60): ...
-    def receive(self, timeout=None): ...
+    def transport(self) -> Literal["polling", "websocket", ""]: ...
+    def emit(
+        self, event: str, data: DataType | tuple[DataType, ...] | None = ...
+    ) -> None: ...
+    def call(
+        self,
+        event: str,
+        data: DataType | tuple[DataType, ...] | None = ...,
+        timeout: int = ...,
+    ) -> Incomplete | None: ...
+    def receive(self, timeout: float | None = ...) -> list[Any]: ...
     def disconnect(self) -> None: ...
-    def __enter__(self): ...
-    def __exit__(self, exc_type: type[BaseException] | None, exc_val: BaseException | None, exc_tb: types.TracebackType | None) -> None: ...
+    def __enter__(self) -> Self: ...
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: types.TracebackType | None,
+    ) -> None: ...
