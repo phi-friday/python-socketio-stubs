e375cf58dc60e6d8c678475c11a1cd45f4ba2eed
phi <phi.friday@gmail.com>
2025-08-02 00:26:23 +0900

    fix: redis_manager

--- src/socketio-stubs/redis_manager.pyi
+++ src/socketio-stubs/redis_manager.pyi
@@ -1,13 +1,31 @@
-from .pubsub_manager import PubSubManager as PubSubManager
+import logging
+from typing import Any
+
 from _typeshed import Incomplete
+from socketio.pubsub_manager import PubSubManager
+from typing_extensions import TypedDict
 
 logger: Incomplete
 
-def parse_redis_sentinel_url(url): ...
+class _RedisArgs(TypedDict, total=False):
+    username: str
+    password: str
+    db: int
+
+def parse_redis_sentinel_url(
+    url: str,
+) -> tuple[list[tuple[str, int]], str | None, _RedisArgs]: ...
 
 class RedisManager(PubSubManager):
     name: str
-    redis_url: Incomplete
+    redis_url: str
     redis_options: Incomplete
-    def __init__(self, url: str = 'redis://localhost:6379/0', channel: str = 'socketio', write_only: bool = False, logger=None, redis_options=None) -> None: ...
+    def __init__(
+        self,
+        url: str = ...,
+        channel: str = ...,
+        write_only: bool = ...,
+        logger: logging.Logger | None = ...,
+        redis_options: dict[str, Any] | None = ...,
+    ) -> None: ...
     def initialize(self) -> None: ...
