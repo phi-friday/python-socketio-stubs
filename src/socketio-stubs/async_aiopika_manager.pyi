8ccf922ed686f6841216c3b9b03711add928952c
phi <phi.friday@gmail.com>
2025-08-02 22:29:27 +0900

    fix: async_aiopika_manager

--- src/socketio-stubs/async_aiopika_manager.pyi
+++ src/socketio-stubs/async_aiopika_manager.pyi
@@ -1,10 +1,22 @@
-from .async_pubsub_manager import AsyncPubSubManager as AsyncPubSubManager
-from _typeshed import Incomplete
+import logging
+
+from aio_pika.abc import (
+    AbstractRobustChannel,
+    AbstractRobustConnection,
+    AbstractRobustExchange,
+)
+from socketio.async_pubsub_manager import AsyncPubSubManager
 
 class AsyncAioPikaManager(AsyncPubSubManager):
     name: str
-    url: Incomplete
-    publisher_connection: Incomplete
-    publisher_channel: Incomplete
-    publisher_exchange: Incomplete
-    def __init__(self, url: str = 'amqp://guest:guest@localhost:5672//', channel: str = 'socketio', write_only: bool = False, logger=None) -> None: ...
+    url: str
+    publisher_connection: AbstractRobustConnection | None
+    publisher_channel: AbstractRobustChannel | None
+    publisher_exchange: AbstractRobustExchange | None
+    def __init__(
+        self,
+        url: str = ...,
+        channel: str = ...,
+        write_only: bool = ...,
+        logger: logging.Logger | None = ...,
+    ) -> None: ...
