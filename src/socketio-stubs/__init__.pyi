01398b6c501fffe91be66ea9acbd944166ada061
phi <phi.friday@gmail.com>
2025-08-02 22:35:50 +0900

    fix: init

--- src/socketio-stubs/__init__.pyi
+++ src/socketio-stubs/__init__.pyi
@@ -1,22 +1,49 @@
-from .asgi import ASGIApp as ASGIApp
-from .async_aiopika_manager import AsyncAioPikaManager as AsyncAioPikaManager
-from .async_client import AsyncClient as AsyncClient
-from .async_manager import AsyncManager as AsyncManager
-from .async_namespace import AsyncClientNamespace as AsyncClientNamespace, AsyncNamespace as AsyncNamespace
-from .async_redis_manager import AsyncRedisManager as AsyncRedisManager
-from .async_server import AsyncServer as AsyncServer
-from .async_simple_client import AsyncSimpleClient as AsyncSimpleClient
-from .client import Client as Client
-from .kafka_manager import KafkaManager as KafkaManager
-from .kombu_manager import KombuManager as KombuManager
-from .manager import Manager as Manager
-from .middleware import Middleware as Middleware, WSGIApp as WSGIApp
-from .namespace import ClientNamespace as ClientNamespace, Namespace as Namespace
-from .pubsub_manager import PubSubManager as PubSubManager
-from .redis_manager import RedisManager as RedisManager
-from .server import Server as Server
-from .simple_client import SimpleClient as SimpleClient
-from .tornado import get_tornado_handler as get_tornado_handler
-from .zmq_manager import ZmqManager as ZmqManager
+from socketio.asgi import ASGIApp as ASGIApp
+from socketio.async_aiopika_manager import AsyncAioPikaManager as AsyncAioPikaManager
+from socketio.async_client import AsyncClient as AsyncClient
+from socketio.async_manager import AsyncManager as AsyncManager
+from socketio.async_namespace import AsyncClientNamespace as AsyncClientNamespace
+from socketio.async_namespace import AsyncNamespace as AsyncNamespace
+from socketio.async_redis_manager import AsyncRedisManager as AsyncRedisManager
+from socketio.async_server import AsyncServer as AsyncServer
+from socketio.async_simple_client import AsyncSimpleClient as AsyncSimpleClient
+from socketio.client import Client as Client
+from socketio.kafka_manager import KafkaManager as KafkaManager
+from socketio.kombu_manager import KombuManager as KombuManager
+from socketio.manager import Manager as Manager
+from socketio.middleware import Middleware as Middleware
+from socketio.middleware import WSGIApp as WSGIApp
+from socketio.namespace import ClientNamespace as ClientNamespace
+from socketio.namespace import Namespace as Namespace
+from socketio.pubsub_manager import PubSubManager as PubSubManager
+from socketio.redis_manager import RedisManager as RedisManager
+from socketio.server import Server as Server
+from socketio.simple_client import SimpleClient as SimpleClient
+from socketio.tornado import get_tornado_handler as get_tornado_handler
+from socketio.zmq_manager import ZmqManager as ZmqManager
 
-__all__ = ['SimpleClient', 'Client', 'Server', 'Manager', 'PubSubManager', 'KombuManager', 'RedisManager', 'ZmqManager', 'KafkaManager', 'Namespace', 'ClientNamespace', 'WSGIApp', 'Middleware', 'AsyncSimpleClient', 'AsyncClient', 'AsyncServer', 'AsyncNamespace', 'AsyncClientNamespace', 'AsyncManager', 'AsyncRedisManager', 'ASGIApp', 'get_tornado_handler', 'AsyncAioPikaManager']
+__all__ = [
+    "ASGIApp",
+    "AsyncAioPikaManager",
+    "AsyncClient",
+    "AsyncClientNamespace",
+    "AsyncManager",
+    "AsyncNamespace",
+    "AsyncRedisManager",
+    "AsyncServer",
+    "AsyncSimpleClient",
+    "Client",
+    "ClientNamespace",
+    "KafkaManager",
+    "KombuManager",
+    "Manager",
+    "Middleware",
+    "Namespace",
+    "PubSubManager",
+    "RedisManager",
+    "Server",
+    "SimpleClient",
+    "WSGIApp",
+    "ZmqManager",
+    "get_tornado_handler",
+]
