9a62f0b4d9fa9866420e75d0134ed2bf63eb8bc4
phi <phi.friday@gmail.com>
2025-08-03 13:28:40 +0900

    fix: MsgPackPacket

--- src/socketio-stubs/msgpack_packet.pyi
+++ src/socketio-stubs/msgpack_packet.pyi
@@ -1,12 +1,7 @@
-from _typeshed import Incomplete
 from socketio.packet import Packet
 from typing_extensions import Buffer
 
 class MsgPackPacket(Packet):
     uses_binary_events: bool
     def encode(self) -> bytes: ...
-    packet_type: Incomplete
-    data: Incomplete
-    id: Incomplete
-    namespace: Incomplete
     def decode(self, encoded_packet: Buffer) -> None: ...  # pyright: ignore[reportIncompatibleMethodOverride]
